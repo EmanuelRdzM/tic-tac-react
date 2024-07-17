ac3cd349746cd15c1548dfeadbb4162302ba846d
EmanuelRdz <j.emanuel.rdz.m@hotmail.com>
2024-07-17 15:09:42 -0600

    initial commint - Tic Tac Toe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>El gatito #</h1>
       </header>
+      <body className='App-body'>
+        <Board></Board>
+      </body>
     </div>
+  );
+}
+
+function Square(){
+  const [value, setvalue] = useState(null); 
+
+  function handleClick(){
+    setvalue('X');
+  }
+
+  return (
+    <button className='square' onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  return (
+    <>
+      <div className='board-row'>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className='board-row'>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className='board-row'>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+    </>
   );
 }
 
